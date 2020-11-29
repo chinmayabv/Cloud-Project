8e67a310ea88b3d6501f0978ae2753c19ba94727
Chinmaya Venkatesh <chinmayavenki2@gmail.com>
2020-11-29 17:37:47 -0600

    script to dataset

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for
-from recognize_video import predictVideo
+from recognize import predictimg
 import os.path
 from werkzeug.utils import secure_filename
+import datetime
+import subprocess
 
-UPLOAD_FOLDER = './dataset/'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = './zipdataset/'
+UPLOAD_FOLDER1 = './dataset/'
+Check_Folder = './classphoto/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif','zip'}
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route("/getstname",methods=['GET'])
+app.config['UPLOAD_FOLDER1'] = UPLOAD_FOLDER1
+app.config['CHECK_FOLDER'] = Check_Folder
+@app.route("/getstname",methods=['GET','POST'])
 def predict():
-	name = predictVideo('face_detection_model','openface_nn4.small2.v1.t7','output/recognizer.pickle','output/le.pickle')
-	return jsonify(name)
+	if request.method == "POST":
+		ts = datetime.datetime.now().timestamp()
+		print(ts)
+		uploaded_files = request.files.getlist("files[]")
+		print(uploaded_files)
+		filenames = []
+		for file in uploaded_files:
+			print("In for")
+			if file and allowed_file(file.filename):
+				# Make the filename safe, remove unsupported chars
+				filename = secure_filename(file.filename)
+				# Move the file form the temporal folder to the upload
+				# folder we setup
+				file.save(os.path.join(app.config['CHECK_FOLDER'], filename))
+				#Save the filename into a list, we'll use it later
+				filepath = Check_Folder+filename
+				name = predictimg('face_detection_model','openface_nn4.small2.v1.t7','output/recognizer.pickle','output/le.pickle',filepath)
+				return redirect(url_for('checkname',stname=name))
+	return render_template('Attendance.html')
 	
-
+@app.route("/CheckName/<stname>",methods=['GET','POST'])
+def checkname(stname):
+	if request.method == "POST":
+		return"<h1> Success</h1>"
+	return render_template('Checkname.html',name=stname)
+		
 @app.route("/Register",methods=['POST','GET'])
 def register():
 	if request.method == "POST":
@@ -41,9 +69,12 @@ def register():
 				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 				#Save the filename into a list, we'll use it later
 				filenames.append(filename)
+		subprocess.call(["./script1.sh"])
 		return render_template('Index.html')
 	return render_template('Register.html')
-	
+
+
+
 @app.route("/Register/<filename>")
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
